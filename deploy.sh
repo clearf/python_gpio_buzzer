90f28c5ec10b94ed15927c399200af64078f430b
Chris Clearfield <chris.clearfield@gmail.com>
2013-07-26 23:49:27 -0400

    chanigng ignores

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,11 @@
 # This is to copy thing over. In addition, to setup the virtualenv in our production space, do:
 # virtualenv prod
 # pip install -r requirements.txt (which lives in the repo but isn't copied)
-
 # hg archive -I . -X "*.txt" -X "*.sh" /var/www/; sudo chown -R www-data:www-data /var/www/buzzer/*
+
+git archive --format tar master | tar -xv -C /var/www/buzzer/pythonws/ 
+sudo chown -R www-data:www-data /var/www/buzzer/*
+sudo chown -R root:www-data /var/www/buzzer/pythonws/buzzer.fcgi
+
 # We've switched to git to deploy. 
 sudo /etc/init.d/lighttpd restart
