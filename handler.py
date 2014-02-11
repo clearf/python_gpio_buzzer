e04fb5c14e8a67293ac7746763f1dd6d0a2af419
Clearfield Christopher <chris.clearfield@gmail.com>
2014-02-10 21:27:49 -0500

    exceptions...

--- handler.py
+++ handler.py
@@ -95,7 +95,7 @@ class Gatekeeper(object):
             subprocess.check_call(["/usr/bin/mpg321", "http://s3-us-west-2.amazonaws.com/hobby.lyceum.dyn.dhs.org/buzzer/r2d2-squeaks2.mp3"])
           except Exception as e:
             log('Exception' + str(e))
-            log(e.output)
+            log(e)
             r.reject("Busy")
         elif self.relay.open_door():
           log("opening")
