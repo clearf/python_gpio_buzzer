75a0c4bf9c060b63d628b16625dd931951e3accd
Chris Clearfield <chris.clearfield@gmail.com>
2014-02-11 20:38:38 -0500

    adding UTF8 encoding for google trans

--- text_to_speech.sh
+++ text_to_speech.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-say() { local IFS=+;/usr/bin/mpg321 "http://translate.google.com/translate_tts?tl=en&q=$*"; }
+say() { local IFS=+;/usr/bin/mpg321 "http://translate.google.com/translate_tts?ie=UTF-8&tl=en&q=$*"; }
 say $*
